31310444afd32fc0973fdb1557c5f75dcfbf5988
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-07-26 19:11:44 +0100

    Adding online images using src

--- lib/main.dart
+++ lib/main.dart
@@ -86,35 +86,10 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: 100.0,
-              width: 100.0,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(25.0),
-                color: Colors.deepPurple,
-              ),
-            ),
-
-            Container(
-              height: 100.0,
-              width: 100.0,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(25.0),
-                color: Colors.deepPurple,
-              ),
-            )
-          ],
-
-        )
-
+      body: Image.network(
+          'https://img.freepik.com/free-photo/anime-eyes-illustration_23-2151660535.jpg',
+           height: double.infinity,
+          fit: BoxFit.cover,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
